bdc3f755521f9d362d889736a09574de93c6c6f4
DeveloperArmando <armando.deveeloper@gmail.com>
2023-07-25 15:54:13 -0600

    Reading env mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,9 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/taskdb'),
-    TasksModule,
     ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+    TasksModule,
   ],
 })
 export class AppModule {}
