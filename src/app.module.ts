67a57591cc13c347b25f0abae0cb224414969ac4
DeveloperArmando <armando.deveeloper@gmail.com>
2023-07-25 15:50:22 -0600

    nest env deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/taskdb'), TasksModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/taskdb'),
+    TasksModule,
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
